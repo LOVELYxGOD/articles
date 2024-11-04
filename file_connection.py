44b40101974f0861156fa678bf50faaa008b5516
LOVELY <kotagindeman@gmail.com>
2024-11-04 17:30:47 +0300

    feat: add function save_articles

--- file_connection.py
+++ file_connection.py
@@ -1,5 +1,15 @@
 import json
 def get_articles():
-    with open("articles.json",'r',encoding='utf-8') as articles:
-    	articles_data = json.load(articles)
+    with open("articles.json",'r', encoding='utf-8') as articles:
+        articles_data = json.load(articles)
     return articles_data
+
+def save_articles(name, text):
+    with open("articles.json", 'r', encoding='utf-8') as file:
+        articles = json.load(file)
+
+    articles[name] = text
+
+    with open("articles.json", 'w', encoding='utf-8') as file:
+        json.dump(articles, file, ensure_ascii=False)
+
